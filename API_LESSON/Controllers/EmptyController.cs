a3597cb597f8b1f421cd24a5dcb07c9ec5eb9b25
NoCookies4You <32069171+NoCookies4You@users.noreply.github.com>
2020-02-19 14:54:17 +0100

    Add project files.

---
+++ API_LESSON/Controllers/EmptyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_LESSON.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmptyController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+
+            return Ok(pokemons);
+        }
+        public static List<Pokemon> pokemons = new List<Pokemon>()
+        {
+            new Pokemon() {Name = "EkaNs", Strenght = 67},
+            new Pokemon() {Name = "bULBasAur", Strenght = 200},
+            new Pokemon() {Name = "Eva-Lena", Strenght = 9001}
+        };
+    }
+}
